244cb7e81d5f24fe986a9dafa670eb57b0a89f5a
Thundasantana <ednilson.sdas@outlook.com>
2024-01-11 21:53:09 -0300

    Backend created

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dslearnbds.entities;
+
+public class Role {
+	
+	private Long id;
+	private String authority;
+	
+	public Role() {
+	}
+
+	public Role(Long id, String authority) {
+		super();
+		this.id = id;
+		this.authority = authority;
+	}
+	
+	
+
+}
